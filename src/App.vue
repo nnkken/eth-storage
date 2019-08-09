a80c6deecaede715c7e50e058c99ba9d634820c0
Chung Wu <chung@like.co>
2019-08-09 18:36:06 +0800

    basic functionalities

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="d in data" v-bind:key="d.txHash">
+      {{ d.txHash }}: {{ d.data }}
+    </div>
+    <button :click="fetch">Refresh</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Web3 from "web3";
+import { abi, address } from "./contract.js";
+
+const FROM_BLOCK = 8300000;
+const ENDPOINT = "https://mainnet.infura.io/v3/53c151ea15184dc69ea07b4d2041ba4e";
+
+const web3 = new Web3(new Web3.providers.HttpProvider(ENDPOINT));
+const Storage = new web3.eth.Contract(abi, address);
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      data: [],
+    };
+  },
+  mounted() {
+    this.fetch();
+  },
+  methods: {
+    async fetch() {
+      console.log("Fetching store data");
+      const events = await Storage.getPastEvents("Data", {
+        fromBlock: FROM_BLOCK,
+        // filter: {
+        //     from: FROM_ADDRESS,
+        // },
+      });
+      console.log("Fetched");
+      const result = [];
+      events.forEach(e => {
+        try {
+          const data = e.returnValues.data;
+          const obj = JSON.parse(data);
+          result.push({ txHash: e.transactionHash, from: e.returnValues.from, data: obj });
+        } catch (err) {
+          console.error(`Error when parsing event:`, e, err);
+        }
+      });
+      this.data = result;
+    },
   }
-}
+};
 </script>
 
 <style>
